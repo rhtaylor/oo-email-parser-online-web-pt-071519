717851a4903581eaadd046c1cfb258ff63d7bbf5
Randy Taylor <randyisbusy@gmail.com>
2019-08-12 07:16:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   def parse
       nu = []
       nu << emails.scan(/\S+[.com]/)
-      nu.flatten.uniq!
+      nu.flatten.uniq
     # if emails.include? ","
     #   x = emails.split(",")
     #   y = x.map do |a|
